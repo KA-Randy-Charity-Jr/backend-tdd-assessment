a0d44f30ffcf12840bf1a97866264748372dc1b3
rakechj <rakechj2@outlook.com>
2020-06-17 21:15:44 -0400

    added my name

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -7,7 +7,7 @@ Students are expected to edit this module, to add more tests to run
 against the 'echo.py' program.
 """
 
-__author__ = "???"
+__author__ = "Randy Charity Jr"
 
 import sys
 import importlib
